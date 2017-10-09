fbf93c5cea47cbbe210e8e45a52d7252ebc2beeb
student <student@localhost.localdomain>
2017-10-09 09:11:51 -0600

    Initial Commit

--- bst.c
+++ bst.c
@@ -1,1 +1,37 @@
-# include 
+#include <stdio.h>/* for printf */
+
+#include <stdlib.h>/* for free() */
+
+#include <string.h> /* for strlen */
+
+#include "llist.h"/* for list operations */
+
+
+
+
+
+/* read no more than limit chars into s, return #chars read.  Doesn't include trailing \n */
+
+int gets_n(char *s, int limit)
+
+{
+
+  char *p = s;/* for indexing into s */
+
+  char c;
+
+  if (fgets(s, limit, stdin)) {
+
+    while ((c = *p) && c != '\n') /* scan p through s until 0 or \n */
+
+      p++;
+
+    if (c == '\n')/* erase \n */
+
+      *p = 0;
+
+  }
+
+    return (p - s);/* #chars read (not including terminator or \n*/
+
+  } 
