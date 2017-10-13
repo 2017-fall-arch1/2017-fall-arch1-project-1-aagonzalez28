8b83fb8fc423e19cd639e647b8c52923e724afae
student <aagonzalez28@miners.utep.edu>
2017-10-13 13:51:56 -0600

    Added a header file

---
+++ bst.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h?
+
+typedef struct bstNode{
+
+  char *string;              /* Definig the char */
+  
+  struct bstNode *left;      /* Definig the left node */
+  struct bstNode *right;     /* Definig the right node */
+  
+}
+  bstNode;
+
+#endif
